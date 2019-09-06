adc9533ec10cbfde4a3c3bf8c6cccc454f0c687a
Chris Duran <chris@duran.bio>
2019-09-07 10:59:37 +1200

    All colors BSD-like, w cyan for dirs (visibility)

--- 15-colors.zsh
+++ 15-colors.zsh
@@ -1,6 +1,9 @@
+#Makes LS Colors in Linux adhere to BSD/Darwin conventions, with Cyan directories (for black BGs)
+LS_COLORS='di=1;36:ln=35:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;43'; export LS_COLORS
 
-LS_COLORS='di=1;36:ln=35:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=36;42:ow=30;42'; export LS_COLORS
+#Makes LS Colors in Linux adhere to BSD/Darwin conventions, with Cyan directories (for black BGs)
+#LS_COLORS='di=1;34:ln=35:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=34;42:ow=30;42'; export LS_COLORS
 
-# LS colors, revert to standard BSD colors (WTF cyan)
-LSCOLORS="Gxfxcxdxbxegedabaggcac"; export LSCOLORS
+#Bluify directories for BSD, if desired
+#LSCOLORS="Exfxcxdxbxegedabagecac"; export LSCOLORS
 CLICOLOR=1; export CLICOLOR
