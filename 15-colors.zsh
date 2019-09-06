0920c8354d5f4a2dd7b2aa6ab0e90194eac2e1aa
Chris Duran <chris@duran.bio>
2019-09-07 11:30:56 +1200

    Should be final colors now, cyanify all the dirs

--- 15-colors.zsh
+++ 15-colors.zsh
@@ -1,9 +1,13 @@
 #Makes LS Colors in Linux adhere to BSD/Darwin conventions, with Cyan directories (for black BGs)
-LS_COLORS='di=1;36:ln=35:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;43'; export LS_COLORS
+LS_COLORS='di=1;36:ln=35:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=30;41:sg=30;43:tw=30;42:ow=30;46'; export LS_COLORS
 
 #Makes LS Colors in Linux adhere to BSD/Darwin conventions, with Cyan directories (for black BGs)
 #LS_COLORS='di=1;34:ln=35:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=34;42:ow=30;42'; export LS_COLORS
 
+#Modify BSD LSCOLORS to keep consistency with directory coloring (non-sticky global accesss now also cyan)
+LSCOLORS="Gxfxcxdxbxegedabadacag"; export LSCOLORS
+
 #Bluify directories for BSD, if desired
 #LSCOLORS="Exfxcxdxbxegedabagecac"; export LSCOLORS
+
 CLICOLOR=1; export CLICOLOR
